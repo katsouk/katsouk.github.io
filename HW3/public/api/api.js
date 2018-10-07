c63d3df5edd46015bf81c40893d1cc84a9394f48
katsouk <33096662+katsouk@users.noreply.github.com>
2018-10-07 18:06:19 -0400

    Update api.js

--- HW3/public/api/api.js
+++ HW3/public/api/api.js
@@ -7,10 +7,10 @@ function create() {
 
         var url = '/account/create/' + name + '/' + email + '/' + password;
         
-        //console.log(name);
-        //console.log(email);
-        //console.log(password);
-        //console.log(url);
+        console.log(name);
+        console.log(email);
+        console.log(password);
+        console.log(url);
         
         superagent
         .get(url)
@@ -18,23 +18,124 @@ function create() {
             if(err){console.log(err)}
             else{console.log(res);
                 status.innerHTML = JSON.stringify(res.body)
-                showAlert(res.body,'Success');
+                showAlert(res.text,'Success');
             }
-        });  
-    }
+        });
+    };
     // -------------------------------------  
     function login() {
 
-        var email     = document.getElementById('email');
-        var password = document.getElementById('password');
-      superagent.get('/account/login/'+email.value+'/'+password.value)
-      .then(function(res) {
-        console.log(res);
-        if(res.type == 'text/html'){
-          showAlert(res.text,'Warning');
-        }
-        else{
-          showAlert(JSON.stringify(res.body),'Success');
-        }
-      });
+        var email    = document.getElementById('loginEmail');
+        var password = document.getElementById('loginPassword');
+        var status   = document.getElementById('status');    
+        var url      = '/account/login/' + email + '/' + password;
+
+        console.log(email);
+        console.log(password);
+        console.log(status);
+
+        superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){console.log(err)}
+            else{console.log(res);
+                status.innerHTML = JSON.stringify(res.text)
+                showAlert(res.text,'Success');
+            }
+        });  
+    };
+    
+    function deposit() {
+        // -------------------------------------
+        var email    = document.getElementById("depositEmail").value;
+        var amount   = document.getElementById("depositAmount").value;
+        var status   = document.getElementById('status').value;    
+        var url      = '/account/deposit/' + email + '/' + amount;
+    
+        console.log(email);
+        console.log(amount);
+        console.log(status);
+
+        superagent
+        .get(url)
+        .end(function(err,res){
+            if(err){console.log(err);}
+            else {console.log(res);}
+            status.innerHTML = JSON.stringify(res.text);
+    });
     }
+    
+    function withdraw() {
+        // -------------------------------------
+        var status   = document.getElementById('status').value;    
+        var email    = document.getElementById("withdrawEmail").value;
+        var amount   = document.getElementById("withdrawAmount").value;
+        var url      = '/account/withdraw/' + email + '/' + amount;
+    
+        console.log(email);
+        console.log(amount);
+        console.log(status);
+
+        superagent
+        .get(url)
+        .end(function(err,res){
+            if(err){console.log(err);}
+            else {console.log(res);}
+            status.innerHTML = JSON.stringify(res.text);
+        });
+    }
+    
+    function transactions() {
+        // -------------------------------------
+        var status   = document.getElementById('status').value;    
+        var email    = document.getElementById("transactionsEmail").value;
+        var url      = '/account/transactions/' + email;
+    
+        console.log(email);
+        console.log(status);
+
+        superagent
+        .get(url)
+        .end(function(err,res){
+            if(err){console.log(err);}
+            else {console.log(res);}
+            status.innerHTML = JSON.stringify(res.text);
+        });
+    }
+    
+    function balance() {
+        // -------------------------------------
+        var status   = document.getElementById('status').value;    
+        var email    = document.getElementById("balanceEmail").value;
+        var url      = '/account/balance/' + email;
+    
+        console.log(email);
+        console.log(status);
+
+        superagent
+        .get(url)
+        .end(function(err,res){
+            if(err){console.log(err);}
+            else {console.log(res);}
+            status.innerHTML = JSON.stringify(res.text);
+        });
+    }
+    
+    function allData() {
+        // -------------------------------------
+            var status  = document.getElementById('status');
+            var url     = '/account/all';
+    
+            console.log(url);
+            console.log(status);
+
+            superagent
+            .get(url)
+            .end(function(err,res){
+                if(err){console.log(err);}
+                else {console.log(res);}
+                status.innerHTML = JSON.stringify(res.body);
+            });
+    }
+    
+    
