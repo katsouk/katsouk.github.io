abf704f825357a7ebdf19280c6421f28319d3db7
katsouk <33096662+katsouk@users.noreply.github.com>
2018-10-07 18:09:04 -0400

    Update index.js

--- HW3/index.js
+++ HW3/index.js
@@ -1,116 +1,135 @@
-//setup server
-var express = require('express');
-var app     = express();
-var low     = require('lowdb');
-var fs      = require('lowdb/adapters/FileSync');
-var adapter = new fs('db.json');
-var db      = low(adapter);
+// SERVER
+    var express = require('express');
+    var app     = express();
+    var low     = require('lowdb');
+    var fs      = require('lowdb/adapters/FileSync');
+    var adapter = new fs('db.json');
+    var db      = low(adapter);
 
-// configure express to serve static files from public directory
-// ------------------------------------------------------------------
-app.use(express.static('public'));
+// DIRECTORY
+    app.use(express.static('public'));
 
-// required data store structure
-// YOUR CODE
+    // DATA STRUCTURE (JSON)
+    db.defaults({accounts:[
+        {name       : 'Kate Soukup',
+        email       : 'ksoukup@mit.edu',
+        balance     : 0,
+        password    : 'password',
+        transactions: []}
+    ]}).write();
+ 
+//Create Accounts tab
+    app.get('/account/create/:name/:email/:password', function (req, res){   
 
-db.defaults(
-    {accounts:[
-            {name        : '',
-             email       : '',
-             balance     : 0,
-             password    : '',
-             transactions: []
-            }]
-    }).write();
-
-
-    app.get('/account/create/:name/:email/:password', function (req, res) {
-
-        var account = {
-            "name"          : name, 
-            "email"         : email, 
-            "balance"       : 0, 
-            "password"      : password, 
-            "transactions"  : []
-        };
-
-        db.get('accounts').push(account).write();
-        console.log(db.get('accounts').value());
-        res.send(db.get('accounts').value());
-
-    });
-        /*
-        
         var name        = req.params.name;
         var email       = req.params.email;
         var password    = req.params.password;
-        var currentacct = getAccount(email, {email: email});
+        //var exists;       
+        //var error;
 
-        
-        if(currentacct.indexOf(email) >= 0){
-            console.log('Account already exists');
-            res.send('Account already exists');
-            return;
-        
-
-    
-    
-
-        app.get('/account/login/:email/:password', function (req, res) {
-            try{
-              var account = db.get('accounts').find({email:req.params.email, password:req.params.password}).value();
-              if(account ==null){
-                res.send('Account not found for '+req.params.email);
-              }else{
-                res.send(account);
-              }
-        
-            }catch (e) {
-              res.send('Account of '+req.params.name+ ' failed :'+e.message);
-            }
-        });
-        
-
-        
-        // return success or failure string
-
-
-        /*
         try{
-          db.get('accounts').push({name:req.params.name,email:req.params.email, balance:0, password:req.params.password,transactions:[]}).write();
-          res.send('Account of '+req.params.name+ ' created');
-        }catch (e) {
-          res.send('Account of '+req.params.name+ ' failed :'+e.message);
-        }
+            db.get('accounts').push({account}).write();      
+            res.send('Created account: ' + email);
+          }catch (e) {
+            res.send('Could not create ' + email);
+          }
+      });
+
+//Create Login tab
+    app.get('/account/login/:email/:password', function (req, res) {
+
+    var email       = req.params.email;
+    var password    = req.params.password;
+
+    try{
+      var account = db.get('accounts').find({email:email, password:password}).value();
+      if(account == null){
+        res.send('Could ot find ' + email);
+      } 
+      else{
+        res.send('Successfully logged into ' + email);
+      }
+    }catch(e) {
+      res.send('Failed to access ' + email);
+    }
     });
-*/
 
+//Get email address
+    app.get('/account/get/:email', function (req, res) {
 
-  /*
-    var accounts = {
-            "name"          : req.params.name;
-            "email"         : req.params.email;
-            "balance"       : 0
-            "password"      : req.params.password;
-            "transactions"  : req.params.transactions;
-          };
+    var email       = req.params.email;
 
-    var acctstatus = getAccount(email, req.params.email);
+    try{
+        var account = db.get('accounts').find({email:email}).value();
+        res.send(account);
+        if(account ==null)
+            {res.send('Could ot find ' + email);}
+        else
+            {res.send(account);}
+    }
+    catch (e) 
+        {res.send('Failed to access ' + email);}
+});
 
-    if(acctstatus != null){
-        res.send(name + " already exists.");
-        return;
+//Deposit money
+    app.get('/account/deposit/:email/:amount', function (req, res) {
+        
+    var email          = req.params.email;
+    var newamount      = req.params.amount;
+    var transactions   = account.transactions;
 
-)};
-*/
+    try{
+      var account = db.get('accounts').find({email:email}).value();
+      //console.log(account);
+      if(account == null){
+        {res.send('Could not find ' + email);}
+      }else{
+        var newamount = parseInt(account.balance) + parseInt(newamount);
+        transactions.push({Action: 'Deposit', Amount: newamount,  Description: "Deposited " + newamount})
+        account = db.get('accounts')
+            .find({ email: email })
+            .assign({balance: newamount, transactions: transactions})
+            .write();
+        res.send('Deposited ' + newamount);
+      }
+    }catch (e) {
+      res.send('Error');
+    }
+});
 
+//Withdraw Money
 
+app.get('/account/withdraw/:email/:amount', function (req, res) {
 
+    var email          = req.params.email;
+    var newamount      = req.params.amount;
+    var transactions   = account.transactions;
 
+    try{
+      var account = db.get('accounts').find({email:email}).value();
+      //console.log(account);
+      if(account == null){
+        {res.send('Could not find ' + email);}
+      }else{
+        var newamount = parseInt(account.balance) - parseInt(newamount);
+        transactions.push({Action: 'Withdraw', Amount: newamount,  Description: "Withdrew " + newamount})
+        account = db.get('accounts')
+            .find({email:email})
+            .assign({balance: newamount, transactions: transactions})
+            .write();
+        res.send('Withdrew ' + newamount);
+      }
+    }catch (e) {
+      res.send('Error');
+    }
+});
+//All Data Tab
+    app.get('/account/all', function (req, res) {
+    
+    var accounts = db.get('accounts').value();
+    res.send(accounts);
+    }); 
 
-
-// start server
-// -----------------------
 app.listen(80, function(){
-    console.log('Running on port 80')
-})
+  console.log('Running on port:80');
+});
